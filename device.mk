563920db1c0e721c93241b0a41c7a6fd7ed19aca
Wind/owZ <windowz414@gnuweeb.org>
2021-08-28 22:20:05 +0300

    teos: keylayout: Nuke Generic.kl
    * It was ignored by build process anyway so it's unnecessary to keep it.
    
    signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

--- device.mk
+++ device.mk
@@ -159,7 +159,6 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
